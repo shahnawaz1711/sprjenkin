174c1e600139ee8497214d8061e9739ed4dbb329
Shahnawaz Shaikh <shahnawaz.perfect@gmail.com>
2024-11-09 11:12:05 +0530

    First Commit

---
+++ src/test/java/p1/SprjenkinApplicationTests.java
@@ -0,0 +1,21 @@
+package p1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprjenkinApplicationTests {
+
+	static Logger logger = LoggerFactory.getLogger(SprjenkinApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("SprMvnApplicationTests contextLoads STARTED.....");
+		assertEquals(true, true);
+	}
+
+}
